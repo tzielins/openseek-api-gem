627ddbeb4259577d8c6ddb344bd4b8331ed18456
quyen <thucquyendn@gmail.com>
2015-02-18 15:44:34 +0100

    others

---
+++ lib/openbis-api-gem.rb
@@ -0,0 +1,9 @@
+require "openbis-api-gem/version"
+
+module Openbis
+  module Api
+    module Gem
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/openbis-api-gem/version.rb
@@ -0,0 +1,7 @@
+module Openbis
+  module Api
+    module Gem
+      VERSION = "0.0.1"
+    end
+  end
+end
