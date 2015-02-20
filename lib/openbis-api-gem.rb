fc79c6ac4d8ef4fbcb1634d9b4fe88ed0995fb94
quyen <thucquyendn@gmail.com>
2015-02-20 13:22:08 +0100

    more generic command

--- lib/openbis-api-gem.rb
+++ lib/openbis-api-gem.rb
@@ -6,43 +6,54 @@ require 'open4'
 
 module Fairdom
   module OpenbisApi
-    JAR_VERSION="0.1.0"
-    DEFAULT_PATH = File.dirname(__FILE__) + "/../jars/openbis-api-#{JAR_VERSION}.jar"
+    class Query
+      JAR_VERSION="0.1.0"
+      DEFAULT_PATH = File.dirname(__FILE__) + "/../jars/openbis-api-#{JAR_VERSION}.jar"
+      ENDPOINT = 'https://openbis-testing.fair-dom.org/openbis'
 
-
-    def query type, id
-      read_with_open4 type, id
-    end
-
-    def query_command type, id
-      command = "java -jar #{DEFAULT_PATH}"
-      command +=  " #{type}"
-      command += " #{id}"
-      command
-    end
-
-    private
-
-    def read_with_open4 type, id
-      output = ""
-      err_message = ""
-      command = query_command type, id
-      status = Open4::popen4(command) do |pid, stdin, stdout, stderr|
-        while ((line = stdout.gets) != nil) do
-          output << line
-        end
-        stdout.close
-
-        while ((line=stderr.gets)!= nil) do
-          err_message << line
-        end
-        stderr.close
+      def initialize(username, password, endpoint=nil)
+        endpoint ||= ENDPOINT
+        @init_command = "java -jar #{DEFAULT_PATH}"
+        @init_command +=  " -e #{endpoint}"
+        @init_command +=  " -u #{username}"
+        @init_command +=  " -pw #{password}"
       end
 
-      if status.to_i != 0
+      def query type, property, property_value
+        command = query_command type, property, property_value
+        read_with_open4 command
       end
 
-      output.strip
+      def query_command type, property, property_value
+        command = @init_command
+        command +=  " -t #{type}"
+        command += " -p #{property}"
+        command += " -pv #{property_value}"
+        command
+      end
+
+      private
+
+      def read_with_open4 command
+        output = ""
+        err_message = ""
+        status = Open4::popen4(command) do |pid, stdin, stdout, stderr|
+          while ((line = stdout.gets) != nil) do
+            output << line
+          end
+          stdout.close
+
+          while ((line=stderr.gets)!= nil) do
+            err_message << line
+          end
+          stderr.close
+        end
+
+        if status.to_i != 0
+        end
+
+        output.strip
+      end
     end
   end
 end
